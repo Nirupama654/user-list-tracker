b982ed629f7cedfba3bc305dc32f11ff46c6cfd1
Nirupama Prasad <nirupama@spikewell.com>
2022-08-17 18:52:42 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './Components/Users/AddUser';
+import UserList from './Components/Users/UserList';
+
 import './App.css';
 
 function App() {
+  const [usersList,setUsersList] = useState([]);
+
+  const addUserHandler = (uName,uAge) =>{
+    setUsersList ((prevUsersList) => {
+      return [...prevUsersList,{name:uName,age:uAge, id : Math.random().toString()},];
+    });
+  };
+
   return (
     <>
-    <AddUser/>
+    <AddUser onAddUser = {addUserHandler}/>
+    <UserList users={usersList}/>
     </>
   );
 }
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-//   const [userList, setUserList] = useState();
-//   const [name, setName] = useState();
-//   const [age, setAge] = useState();
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
 
-//   const getName = (event) => {
-//     setName(event.target.value);
-//   };
-//   const getAge = (event) => {
-//     setAge(event.target.value);
-//   };
   const addUserHandler = (event) => {
     event.preventDefault();
-    // const obj = {
-    //   username: name,
-    //   age: age,
-    // };
-    // setUserList(...userList, obj);
-    // console.log(userList);
+    // if (enteredUserName.trim().length === 0 || enteredAge.trim().length) {
+    //     return;
+    // }
+    // if (enteredAge < 1) {
+    //     //converting string to number
+    //     return;
+    // }
+    console.log("clicked");
+    console.log(enteredAge,enteredUserName);
+    props.onAddUser(enteredUserName,enteredAge);
+    setEnteredUserName('');
+    setEnteredAge('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age(in years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          onChange={userNameChangeHandler}
+          value={enteredUserName}
+        />
+        <label htmlFor="age">Age(in years)</label>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
